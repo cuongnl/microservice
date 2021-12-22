635f17b547d08ab394fd8c040ba86839ede7ac1f
Nguyen Lien Cuong <nguyenliencuong@techasians.com>
2021-12-22 18:13:36 +0700

    add source

---
+++ galleryService/src/test/java/com/app/galleryService/GalleryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.galleryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ imageService/src/test/java/com/app/imageService/ImageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.imageService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
